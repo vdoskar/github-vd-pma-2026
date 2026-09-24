a80174282b6717ca183a68ffbd43936c6a5c0997
vdoskar <vladadoskar@gmail.com>
2026-09-24 12:06:15 +0200

    fix

--- 00-test/app/src/main/java/com/example/a00_test/MainActivity.kt
+++ 00-test/app/src/main/java/com/example/a00_test/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a00_test.R
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
